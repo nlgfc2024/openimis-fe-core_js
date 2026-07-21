41988bec2b2f5d4b3909be4a2f7dd93666c7fee3
Elvis <elvisnamuru@gmail.com>
2026-07-21 13:08:05 +0200

    fix: prevent false session expiration on login

--- src/components/App.js
+++ src/components/App.js
@@ -172,7 +172,7 @@ const App = (props) => {
                   onLogout={onLogout}
                 />
               ) : null}
-              <PublishedComponent pubRef="grievanceSocialProtection.GrievanceConfigurationDialog" rights={rights} />
+
               <div className="App">
                 {auth.isAuthenticated && <Contributions contributionKey={APP_BOOT_CONTRIBUTION_KEY} />}
                 <BrowserRouter basename={basename}>
